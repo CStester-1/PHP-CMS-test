1941ccf7c664543e3543303882165f1b6e7c4d0d
tproho1 <44259164+tproho1@users.noreply.github.com>
2019-12-13 14:07:02 +0100

    Categories are now shown in sidebar section of site. Changes made in sidebar.php
    Fixed bug regarding search engine for posts, now it shows only posts that gave required tag.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,28 +1,13 @@
 <div class="col-md-4">
-    <?php
-    if(isset($_POST['submit'])){
-        $search = $_POST['search'];
-        $query = "SELECT * FROM posts WHERE post_tag LIKE '%$search%'";
-        $searchQuery = mysqli_query($connection, $query);
 
-        if(!$searchQuery){
-            die("QUERY FAILED" . mysqli_error($connection));
-        }
-
-        $count = mysqli_num_rows($searchQuery);
-
-        if($count == 0){
-            echo "No tags related to your search!";
-        }
-    }
-
-    ?>
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="post">
+        <form action="search.php" method="post">
         <div class="input-group">
-            <input name="search" type="text" class="form-control">
+            <label>
+                <input name="search" type="text" class="form-control">
+            </label>
             <span class="input-group-btn">
                 <button name="submit" class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
@@ -36,18 +21,21 @@
 
     <!-- Blog Categories Well -->
     <div class="well">
+        <?php
+        $query = "SELECT * FROM categories";
+        $selectAllCategoriesQuery = mysqli_query($connection, $query);
+
+        ?>
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+                    while($row = mysqli_fetch_assoc($selectAllCategoriesQuery)){
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    }
+                    ?>
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
@@ -76,4 +64,3 @@
 
 </div>
 
-</div>
